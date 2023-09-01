7271b79eee9e72d371a604d4fd3ab19bab4698eb
mdrt1395 <marcodavidrt95@gmail.com>
2023-08-31 21:43:12 -0600

    Finished NoteDatabase.kt

--- app/src/main/java/com/example/notesapp/Database/NoteDatabase.kt
+++ app/src/main/java/com/example/notesapp/Database/NoteDatabase.kt
@@ -1,13 +1,39 @@
 package com.example.notesapp.Database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import com.example.notesapp.Models.Note
 
 //ask about all these things
 
 @Database(entities = arrayOf(Note::class), version = 1, exportSchema = false)
-class NoteDatabase {
+abstract class NoteDatabase {
 
-    abstract fun
+    abstract fun getNoteDao() : NoteDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: NoteDatabase? = null
+
+        fun getDataBase(content: Context) : NoteDatabase{
+
+            return INSTANCE?: synchronized(this){
+
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteDAtabase::class.java,
+                    DATABASE_NAME
+                ).build()
+
+                INSTANCE = instance
+
+                instance
+            }
+
+        }
+
+    }
 
 }
