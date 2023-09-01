c3b05a234de7f7f58185461643988813e166a81c
mdrt1395 <marcodavidrt95@gmail.com>
2023-09-01 12:54:42 -0600

    Updated colors.xml

--- app/src/main/java/com/example/notesapp/Adapter/NotesAdapter.kt
+++ app/src/main/java/com/example/notesapp/Adapter/NotesAdapter.kt
@@ -2,6 +2,7 @@ package com.example.notesapp.Adapter
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.cardview.widget.CardView
@@ -19,7 +20,7 @@ class NotesAdapter(private val context: Context) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
         return NoteViewHolder(
-            LayoutInflater.from(context).inflate((R.layout.list_item, parent, false))
+            LayoutInflater.from(context).inflate((R.layout.list_item), parent, false))
         )
     }
 
@@ -30,15 +31,22 @@ class NotesAdapter(private val context: Context) :
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         val currentNote = NotesList[position]
         holder.title.text = currentNote.title
+        holder.title.isSelected = true
+
         holder.Note_tv.text = currentNote.note
         holder.date.text = currentNote.date
+        holder.date.isSelected = true
+    }
+
+    fun RandomColor(): Int{
+
     }
 
     inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         val notes_layout = itemView.findViewById<CardView>(R.id.card_layout)
         val title = itemView.findViewById<TextView>(R.id.tv_title)
-        val note =itemView.findViewById<TextView>(R.id.tv_note)
+        val Note_tv =itemView.findViewById<TextView>(R.id.tv_note)
         val date = itemView.findViewById<TextView>(R.id.tv_date)
 
 
